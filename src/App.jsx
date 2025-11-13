57eb3116c6dfe8e6f9386224bb8d55fb83c2bab1
sanjyalsmriti <sanjyalsmriti@gmail.com>
2025-11-13 06:20:27 +0545

    react router dom added and page can be viewd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import RomanConverterPage from './pages/RomanConverterPage'
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/roman" element={<RomanConverterPage />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
